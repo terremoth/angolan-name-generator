4b183f22540a97d6ecfb8a6b1a60171ee1c12553
Lucas M. Dutra <terremoth@users.noreply.github.com>
2024-07-17 15:33:28 -0300

    Add many names

--- generate.js
+++ generate.js
@@ -4,28 +4,119 @@ const names_before = `Dolar
 Plufit Victory
 Mestr@ Mágica
 Sua Excelencia
+Djanera
+Insurret@
+Da Rosa
+@ Letrad@
 Vem ser famos@
+Miúd@
+Ativad@
+Esmerald@
+Fuyame
+Doming@s
+Tonivald@
+Pay Loc@
+Baraka
+De Muanza
+Figura Publica
+Star Best
+Figurå
+Sacapela
+Ölïvëïrä
+Ausente Sempre
+Modo Infinito
+Båd
+Blend
+Åcümüläd@
+Kituxi
+Edy@
+Cara de Anjo
+Liberdade
+Constantin@
+Rei Salazar
 @ Mestre de Todos
-Mamba Jack`;
+@ Pacifico
+Mamba Jack
+Moren@ Sortud@`;
 
 const names_after = `Selestin@
 Smoke
 Biguliulson
-Tafinha
+Tafinha Tg
 Kissanga
+Modo Bumba
 Querid@
 Medusa
 Classic@
+Kid King
 @ Princes@
 Golfin@
 @ Mystik@
+Kusseka
+Cooktail Balanga
+Skill Paris King
+Mäjor Mustang
 Amoros@ Valente
 Victory Victory
+Canjica de Ouro
+Kipaca Mamboma
+Cumbe Rapin@
+Chef Bakongo
 Setas NBA
+Sölótchï
+Chapelin@
 @ Reformad@
+Los Compadres
 El Dracula
+@ Bitinh@
+Love Lov
+Principiad@ de Eldorado
 Peregrin@
-Boss
+Barba de Gato
+Galátic@
+Ruas das Ruas
+Missed King
+Djucci Ginga
+Mu Disso
+Djudja Nganda
+Boss Januário
+Sänämä
+De Marsel@
+Black Pausad@
+Tonilson
+Malungo
+Serafim Tchissoca
+Coroa Ndombele
+@ Mestrad@
+@ Hilaria
+Destruça Sacrifício
+Manda Brazza Kg
+Muandumba Deus
+Kunzaila Raper
+Zulu Matiãs
+Martinh@ Reformad@
+Labate Animador
+dos Sangradores
+Chad Patrici@
+Rico Bilha
+Storrbiny
+Prëdëstinäd@
+Domingos Dady King
+Moi Sés Ouro
+Muachissanda
+Honrad@ Bill Key
+Filh@ de Deus Pai
+Lampião Lampião
+Malta Académica
+Zecas Kelson
+Roda Smock
+Nagga Ng
+De Deus Friend
+Mangovo Malta
+Oficial da Costeira
+Cönqüstädör@
+Do Mundinho
+Trezentinh@
 Estabilidade Evidente
 do Kuduro`;
 
